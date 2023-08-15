19f47426fe145195992bfe03b4ef510401baf468
Andrey <priv1984@yandex.ru>
2023-08-16 00:15:42 +0700

    doubleRun.js

--- doubleRun.js
+++ doubleRun.js
@@ -35,8 +35,7 @@ let link = 'https://www.dns-shop.ru/catalog/17aa00d516404e77/nabory-ruchnogo-ins
         await page.waitForSelector('a.catalog-product__image-link', {timeout: 5000})
             .then(async () => {
 
-                console.log('SUCCESS');
-                console.log(counter);
+
                 let html = await page.evaluate(async () => {
                     let page = [];
                     let divs = document.querySelectorAll('div.ui-button-widget');
@@ -51,32 +50,33 @@ let link = 'https://www.dns-shop.ru/catalog/17aa00d516404e77/nabory-ruchnogo-ins
 
                 res = res.concat(html);
                 counter++;
-                console.log('length = ' + html.length);
+                console.log('SUCCESS / ' + counter + ' / ' + html.length);
             }).catch(e => {
                 flag--;
-                console.log('FALSE = ' + flag);
+                console.log('== FALSE ==');
             });
     }
 
-    let binaryWS = XLSX.utils.json_to_sheet(res);
+/*    let binaryWS = XLSX.utils.json_to_sheet(res);
     let wb = XLSX.utils.book_new()
     XLSX.utils.book_append_sheet(wb, binaryWS, 'Noutbuki')
-    XLSX.writeFile(wb, 'DNS_shop.xlsx');
+    XLSX.writeFile(wb, 'DNS_shop.xlsx');*/
 
     await browser.close()
     let end = Date.now();
-    console.log(end - start);
+    console.log((end - start)/1000);
    await double(res);
 })();
 
 
 
 let double = async (arrSecondRounds) => {
-
+    let start = Date.now();
     let flag = 4;
     let res = [];
     let counter = 0;
-    let arrSecondRound = arrSecondRounds
+    let arrSecondRound = arrSecondRounds;
+    let totalElements = arrSecondRound.length;
 
 
     let browser = await puppeteer.launch({
@@ -94,8 +94,7 @@ let double = async (arrSecondRounds) => {
         await page.goto(`${arrSecondRound[counter]}`);
         await page.waitForSelector('div.product-card-description-text', {timeout: 15000})
             .then(async () => {
-                console.log('SUCCESS');
-                console.log(counter);
+
                 let html = await page.evaluate(async () => {
                     let page = [];
                     let name = document.querySelector('h1.product-card-top__title').innerText;
@@ -119,11 +118,12 @@ let double = async (arrSecondRounds) => {
                 }, {waitUntil: 'load'});
                 res = res.concat(html);
                 counter++;
-                if (arrSecondRound.length == counter) {
+                if (arrSecondRound.length == counter || counter == 10) {
                     flag = 0
                 }
+                console.log('SUCCESS / ' + counter + '/' + totalElements);
             }).catch(e => {
-                console.log('FALSE = ' + flag);
+                console.log('== FALSE ==');
             });
     }
 
@@ -134,7 +134,8 @@ let double = async (arrSecondRounds) => {
 
     console.log(res)
     await browser.close()
-
+    let end = Date.now();
+    console.log((end - start)/1000);
 };
 
 
